ff63bac1903aeae3eb8a7d40d9e10dd5e8f37f19
Alex Marcel <nitan.marcel@gmail.com>
2021-07-22 18:33:26 +0300

    bump version from v0.0.3 to v0.0.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt") as reqs:
 
 setuptools.setup(
     name="tl_radio",
-    version="0.0.3",
+    version="0.0.4",
     url="https://github.com/nitanmarcel/tl_radio",
 
     author="Marcel Alexandru Nitan",
